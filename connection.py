1bcd2664952442861c54301a9994bacb039ad99a
aboodaljrafe <abdullrahmantowfig@gmail.com>
2026-07-24 12:10:42 +0300

    Update connection.py

--- connection.py
+++ connection.py
@@ -3,11 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 from config import Config
-from models import Base  # استدعاء مباشر من الجذر
+from models import Base
+
+db_url = Config.DATABASE_URL if hasattr(Config, 'DATABASE_URL') and Config.DATABASE_URL else "sqlite:///contest_bot.db"
 
 engine = create_engine(
-    Config.DATABASE_URL,
-    connect_args={"check_same_thread": False} if "sqlite" in Config.DATABASE_URL else {}
+    db_url,
+    connect_args={"check_same_thread": False} if "sqlite" in db_url else {}
 )
 
 SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
